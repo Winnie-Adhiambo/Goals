01bf5d5300010da01165705e7a518bfc12f4ff94
MoringaSchool <winnieadhiambo600@gmail.com>
2018-10-19 11:13:16 +0300

    added content on html file

---
+++ src/app/goal-details/goal-details.component.ts
@@ -0,0 +1,40 @@
+// import { Component, OnInit,Input } from '@angular/core';
+// import {Goal} from '../goal'
+//
+// @Component({
+//   selector: 'app-goal-details',
+//   templateUrl: './goal-details.component.html',
+//   styleUrls: ['./goal-details.component.css']
+// })
+// export class GoalDetailsComponent implements OnInit {
+//
+//   @Input() goal:Goal;
+//   constructor() { }
+//
+//   ngOnInit() {
+//   }
+//
+// }
+
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import {Goal} from '../goal'
+
+@Component({
+  selector: 'app-goal-details',
+  templateUrl: './goal-details.component.html',
+  styleUrls: ['./goal-details.component.css']
+})
+export class GoalDetailsComponent implements OnInit {
+
+  @Input() goal:Goal;
+  @Output() isComplete= new EventEmitter<boolean>();
+
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
